dc38a20a257f7b1cc1399f0ffbc1613c5463918f
wychesterso <chesterwyso@gmail.com>
2025-06-24 11:54:36 +1000

    Using in-memory SQLite and overrode DB dependency for isolation

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,23 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.database import Base, get_db, URL
 from app.main import app
 
+SQLALCHEMY_TEST_DATABASE_URL = "sqlite:///:memory:"
+engine = create_engine(SQLALCHEMY_TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+app.dependency_overrides[get_db] = override_get_db
 client = TestClient(app)
 
 def test_shorten_url():
